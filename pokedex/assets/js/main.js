f0104f25b275ac85a4574d38016763b3df627c00
Debora Silva <deboraro.61@gmail.com>
2023-04-02 20:18:46 -0300

    primeira requisição

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,1 +1,7 @@
-console.log("Sucesso")
+const offset = 0 ;
+const limit = 10 ;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+fetch(url)
+.then((response)=>response.json())
+.then((res)=>console.log(res))

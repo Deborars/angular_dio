40cedd813de37ffed6ecd33c215ceccd8b810a6b
Debora Silva <deboraro.61@gmail.com>
2023-04-03 20:57:11 -0300

    separando o consumo da API da manipulação de HTML

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0 ;
-const limit = 10 ;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 function convertPokemonToLi(pokemon){
   return `
@@ -18,13 +16,7 @@ function convertPokemonToLi(pokemon){
 `
 }
 
-fetch(url)
-.then((response)=>response.json())
-.then((response)=>response.results)
-.then((pokemonList)=>{
-
-// debugger
-// console.log(pokemonList)
+pokeApi.getPokemons().then((pokemonList)=>{
 
   for(let i = 0; i < pokemonList.length; i++){
     const pokemon = pokemonList[i];

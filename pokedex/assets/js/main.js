cffd509976d3956697bc166271dcbd81dd54ab7a
Debora Silva <deboraro.61@gmail.com>
2023-04-08 17:44:41 -0300

    criando mecanismo de paginação

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,35 +1,48 @@
 
+const pokemonList = document.querySelector("#pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton")
+const limit =  5;
+let offset = 0;
 
 
-function convertPokemonToLi(pokemon){
-  return `
-        <li class="pokemon ${pokemon.type}">
-          <span class="number">#0${pokemon.number}</span>
-          <span class="name">${pokemon.name}</span>
-          <div class="detail">
-            <ol class="types">
-              ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join("")}
-            </ol>
-            <img src="${pokemon.photo}" alt="${pokemon.name}" srcset="">
-          </div>
-      </li>
-`
+
+
+function loadPokemonItens(offset, limit){
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = [])=>{
+
+      const newList = pokemons.map((pokemon)=>{
+        return  `
+                  <li class="pokemon ${pokemon.type}">
+                    <span class="number">#0${pokemon.number}</span>
+                    <span class="name">${pokemon.name}</span>
+                    <div class="detail">
+                      <ol class="types">
+                        ${pokemon.types.map((type)=> `<li class="type ${type}">${type}</li>`).join("")}
+                      </ol>
+                      <img src="${pokemon.photo}" alt="${pokemon.name}" srcset="">
+                    </div>
+                </li>
+          `
+      })
+
+      //o join pega um array e transforma em uma string por default ele separa por virgula
+    //ai foi colocado sem espacos para ficar sem separador nenhum
+      const newHTML = newList.join('');
+
+      pokemonList.innerHTML += newHTML;
+
+
+    })
+
 }
 
-pokeApi.getPokemons().then((pokemons = [])=>{
+loadPokemonItens(offset, limit);
 
-  const newList = pokemons.map((pokemon)=>{
-    return convertPokemonToLi(pokemon);
-  })
-
-  //o join pega um array e transforma em uma string por default ele separa por virgula
-//ai foi colocado sem espacos para ficar sem separador nenhum
-  const newHTML = newList.join('');
-
-  document.querySelector("#pokemonList").innerHTML += newHTML;
-
-
+loadMoreButton.addEventListener("click", () =>{
+  offset += limit;
+  loadPokemonItens(offset, limit);
 })
-.catch((error)=>console.log(error))
+
 
 
